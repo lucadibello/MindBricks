421845931b44f6abf5e044dac222182ac45adf60
Luca Di Bello <lucadibello@proton.me>
2025-11-17 15:02:29 +0100

    fix: fixed old reference type

--- android/app/src/main/java/ch/inf/usi/mindbricks/ui/onboarding/page/OnboardingNotificationsFragment.java
+++ android/app/src/main/java/ch/inf/usi/mindbricks/ui/onboarding/page/OnboardingNotificationsFragment.java
@@ -1,16 +1,15 @@
 package ch.inf.usi.mindbricks.ui.onboarding.page;
 
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.material.button.MaterialButton;
 
 import ch.inf.usi.mindbricks.R;
 
@@ -23,7 +22,7 @@ public class OnboardingNotificationsFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_onboarding_notifications, container, false);
 
-        Button button = view.findViewById(R.id.buttonEnableNotifications);
+        MaterialButton button = view.findViewById(R.id.buttonEnableNotifications);
         button.setOnClickListener(v -> requestNotificationPermissionIfNeeded());
 
         return view;
