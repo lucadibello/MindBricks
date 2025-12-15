00fcad5b2c9cb55d7e816e4855c8e50401349850
Loca0307 <loca2gaming@gmail.com>
2025-12-15 22:35:09 +0100

    adjusted zoom sens

--- android/app/src/main/java/ch/inf/usi/mindbricks/ui/nav/home/city/IsometricCityView.java
+++ android/app/src/main/java/ch/inf/usi/mindbricks/ui/nav/home/city/IsometricCityView.java
@@ -106,7 +106,7 @@ public class IsometricCityView extends View {
     /**
      * Multiplicative factory for zoom sensitivity (lower = less)
      */
-    private static final float ZOOM_SENSITIVITY_FACTOR = 0.5f;
+    private static final float ZOOM_SENSITIVITY_FACTOR = 0.1f;
 
     /**
      * Current pan offsets for panning the view.
